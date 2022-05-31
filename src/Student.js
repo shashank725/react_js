c1fd1030f816fe74d30d772b06a384b896f76832
shashank725 <shashank.nov725@gmail.com>
2022-05-31 22:22:23 +0530

    Function & Class components

---
+++ src/Student.js
@@ -0,0 +1,15 @@
+import React, { Component} from "react";
+
+// // Function Component
+// const Student = () => {
+//     return <h1>Hello Shashank</h1>
+// }
+
+// Class Component
+class Student extends Component {
+    render(){
+        return <h1>Hello Shashank Shekhar</h1>;
+    }
+}
+
+export default Student;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+// import App from "./App";
+import Student from "./Student";
 
 // #Both does the same thing
 // let el = <h1>Hello Shashank</h1> //JSX
 // let el = React.createElement("h1", null, "Hello Shashank"); //
 
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(<App />, document.getElementById("root")); // To render class component in App.js
+ReactDOM.render(<Student />, document.getElementById("root")); // To render component in Student.js
